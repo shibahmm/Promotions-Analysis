800655a092cce82fa81d323fdb28e54f4e5a67b2
Khang Le <le000326@umn.edu>
2025-03-05 17:05:18 -0600

    packages install and add gitignore

--- analysis.py
+++ analysis.py
@@ -8,4 +8,36 @@ path = kagglehub.dataset_download("ahsan81/superstore-marketing-campaign-dataset
 print("Path to dataset files:", path)
 
 
-#Marketing Campaign Analysis: Chi-squared approach
+#Marketing Campaign Analysis using Chi-Squared Test
+
+#package install: sklearn is deprecated, pip install scikit-learn instead
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import scipy.stats as stats
+from scipy.stats import chi2_contingency
+
+import statsmodels.api as sm
+from statsmodels.stats.multitest import multipletests
+
+from sklearn.preprocessing import StandardScaler,OneHotEncoder, LabelEncoder
+from sklearn.metrics import roc_curve, roc_auc_score, confusion_matrix, classification_report
+from sklearn.model_selection import train_test_split, GridSearchCV, cross_val_score, KFold
+from sklearn.impute import SimpleImputer
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+
+import plotly.graph_objs as go
+import plotly.io as pio
+
+import os
+for dirname, _, filenames in os.walk('/kaggle/input'):
+    for filename in filenames:
+        print(os.path.join(dirname, filename))
+
+
+
+
